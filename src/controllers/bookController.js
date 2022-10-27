4fc6ab7382fd9095f2d17a0f113442392d68b1be
ayus <ayushbirla71@gmail.com>
2022-10-27 18:26:24 +0530

    mongodb session 3

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,5 +1,8 @@
 const { count } = require("console")
 const BookModel= require("../models/bookModel")
+const BookLsModel= require("../models/boolLsModel")
+const autherModel= require("../models/autherLs")
+
 
 const createBook= async function (req, res) {
     let data= req.body
@@ -41,7 +44,46 @@ const deleteBooks= async function (req, res) {
      
      res.send( { msg: allBooks})
 }
+////////////////////////////////////////////////////////////////////////////////
+const getBookLsCreate=async function(req, res){
+    let data=req.body
+    let list =await BookLsModel.create(data)
+    res.send({msg:list})
+}
 
+const getAutherLs=async function(req, res){
+    let auther=req.body
+    let autherList=await autherModel.create(auther)
+    res.send({msg:autherList})
+}
+
+const getautherbook=async function (req, res){
+        let authorId =await autherModel.findOne({author_name:"Ramanujan"}).select({author_id:1, _id:0})
+
+        let bookName=await BookLsModel.find(authorId).select({bookName:1})
+
+    
+      res.send({msg: bookName})
+    }
+const getUser1= async function (req , res){
+    let user=await BookLsModel.findOne({bookName:"Tow states"}).select({author_id:1, _id:0})
+    let updatPrice=await BookLsModel.findOneAndUpdate(user,{$set:{prices:100}},{new:true}).select({prices:1, _id:0})
+
+    let autherOfThisBook=await autherModel.findOne(user).select({author_name:1, _id:0})
+    res.send({updatedPrice : updatPrice.prices, autherOfThisBook:autherOfThisBook.author_name})
+
+}
+
+const getUserData= async function(req, res){
+    let userAll=await BookLsModel.find({prices:{$gte:50, $lte:100}}).select({author_id:1, _id:0})
+    let userAll1=await BookLsModel.find({prices:{$gte:50, $lte:100}}).select({bookName:1, _id:0})
+    let arr=[];
+    for(let i=0; i<userAll.length; i++){
+        let arr2=await autherModel.findOne(userAll[i]).select({author_name:1, _id:0});
+        arr.push(arr2);
+    }
+    console.log(arr, userAll1)
+}
 
 
 
@@ -57,3 +99,8 @@ module.exports.createBook= createBook
 module.exports.getBooksData= getBooksData
 module.exports.updateBooks= updateBooks
 module.exports.deleteBooks= deleteBooks
+module.exports.getBookLsCreate=getBookLsCreate
+module.exports.getAutherLs=getAutherLs
+module.exports.getautherbook=getautherbook
+module.exports.getUser1=getUser1
+module.exports.getUserData=getUserData
--- src/routes/route.js
+++ src/routes/route.js
@@ -23,12 +23,6 @@ router.post("/deleteBooks", BookController.deleteBooks)
 const moment = require('moment');
 router.get("/dateManipulations", function (req, res) {
     
-    // const today = moment();
-    // let x= today.add(10, "days")
-
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
-    
     const dateA = moment('01-01-1900', 'DD-MM-YYYY');
     const dateB = moment('01-01-2000', 'DD-MM-YYYY');
 
@@ -37,5 +31,17 @@ router.get("/dateManipulations", function (req, res) {
 
     res.send({ msg: "all good"})
 })
+////////////////////////////////////////////////////////////////////
+
+
+router.post("/getBookLsCreate", BookController.getBookLsCreate)
+
+router.post("/getAutherLs", BookController.getAutherLs)
+
+router.get("/getautherbook", BookController.getautherbook)
+
+router.get("/getUser1",BookController.getUser1)
+
+router.get("/getUserData",BookController.getUserData)
 
 module.exports = router;
