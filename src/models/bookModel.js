4b2929c73c1bc336cd4cb2d890887ecfb2ef2084
ayushbirla71 <ayushbirla71@gmail.com>
2022-10-29 20:42:03 +0530

    new assignmetn

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,15 +3,26 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    author: {
+        required:true,
         type: ObjectId,
-        ref: "Author"
+        ref: "newAuthor"
     }, 
     price: Number,
-    ratings: Number
+    ratings: Number,
+    publisher: {
+        required:true,
+        type: ObjectId,
+        ref:"newPublisher"
+    },
+    isHardCover:{
+        type:Boolean,
+        default:false
+    }
+
 
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('newBook', bookSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
+const publisherController= require("../controllers/publisherController")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -16,6 +18,16 @@ router.post("/createBook", bookController.createBook  )
 
 router.get("/getBooksData", bookController.getBooksData)
 
+router.post("/createPublisher", publisherController.createPublisher)
+
+router.get("/getPublisherData", publisherController.getPublishersData)
+
 router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+
+router.get("/getBooksWithAuthorAndPublisherDetails", bookController.getBooksWithAuthorAndPublisherDetails)
+
+router.put("/BookPrintBy",bookController.BookPrintBy)
+
+router.put("/updatPrice",bookController.updatPrice)
 
 module.exports = router;
