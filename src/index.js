f13665f4d8d750eecc614f289e84edf23c34df9c
ayushbirla71 <ayushbirla71@gmail.com>
2022-11-06 15:42:20 +0530

    assignmetn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ayush8120:GeGo5qhr7wM6VQyg@cluster0.n1nevi5.mongodb.net/assignment-auth-2?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,14 @@
-const authenticate = function(req, req, next) {
+const jwt=require("jsonwebtoken")
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
+    let token = req.headers["x-auth-token"]
+    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+
+    req.decodedToken=decodedToken
 
     next()
 }
@@ -8,5 +16,14 @@ const authenticate = function(req, req, next) {
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
-    next()
+    let decodedToken=req.decodedToken.userId
+    let userIdDetail= req.params.userId
+    if(decodedToken!==userIdDetail){
+        return res.send ("not allowed to updat other user details")
+
+    }
+
+    else {next()}
 }
+module.exports.authenticate=authenticate
+module.exports.authorise=authorise
