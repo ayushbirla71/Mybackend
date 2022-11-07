4eb6c488c38cf19424fd3087b062858f4a591b5d
ayushbirla71 <ayushbirla71@gmail.com>
2022-11-07 18:52:48 +0530

    trym & catch

--- src/commonMW/middilwear.js
+++ src/commonMW/middilwear.js
@@ -1,17 +1,23 @@
 const jwt = require("jsonwebtoken");
 const commonMid = async function (req, res, next) {
-let token = req.headers["x-auth-token"];
-if (!token) token = req.headers["x-auth-token"];
-   if (!token) return res.send({ status: false, msg: "token must be present" });
-  
-  console.log(token);
+  try {
+    let token = req.headers["x-auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
+
+    // console.log(token);
 
 
-  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-  console.log(decodedToken)
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+    let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
+    //console.log(decodedToken)
+    if (!decodedToken)
+      return res.status(400).send({ status: false, msg: "token is invalid" });
     next()
+  }
+
+  catch (errer) {
+    res.status(500).send({ mag: errer.message })
+  }
 }
 
 module.exports.commonMid=commonMid
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,84 +2,112 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (abcd, xyz) {
-  let data = abcd.body;
-  let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
-};
+  try {
+    let data = abcd.body;
+    let savedData = await userModel.create(data);
+    console.log(abcd.newAtribute);
+    xyz.status(201).send({ msg: savedData });
+  }
+  catch(errer) {
+    xyz.status(400).send({ mag:errer.message })
+
+  }
+}
+
 
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
 
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FunctionUp",
-    },
-    "functionup-plutonium-very-very-secret-key"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.status(400).send({
+        status: false,
+        msg: "username or the password is not corerct",
+      });
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "thorium",
+        organisation: "FunctionUp",
+      },
+      "functionup-plutonium-very-very-secret-key"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(201).send({ status: true, token: token });
+  }
+  catch(errer) {
+    res.status(500).send({ mag:errer.message })
+
+  }
 };
 
 const getUserData = async function (req, res) {
-  
 
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
+  try {
+    let userId = req.params.userId;
+    let userDetails = await userModel.findById(userId);
+    if (!userDetails)
+      return res.status(400).send({ status: false, msg: "No such user exists" });
 
-  res.send({ status: true, data: userDetails });
- 
+    res.status(200).send({ status: true, data: userDetails });
+  }
+  catch(errer) {
+    res.status(500).send({ mag:errer.message })
+
+  }
+
 };
 
 const updateUser = async function (req, res) {
 
-  let userId = req.params.userId;
- 
-  let user = await userModel.findById(userId);
-  console.log(user)
-  
-  if (!user) {
-    return res.send("No such user exists");
-  }
+  try {
+    let userId = req.params.userId;
 
-  let userData = req.body;
-  console.log(userData)
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData ,{new:true});
-  res.send({ status: userData, data: updatedUser });
+    let user = await userModel.findById(userId);
+    console.log(user)
+
+    if (!user) {
+      return res.status(400).send("No such user exists");
+    }
+
+    let userData = req.body;
+    console.log(userData)
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
+    res.status(201).send({ status: userData, data: updatedUser });
+  }
+  catch(errer) {
+    res.status(500).send({mag:errer.message })
+  }
 };
 
 const DeletUser = async function (req, res) {
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  //Return an error if no user with the given id exists in the db
-  if (!user) {
-    return res.send("No such user exists");
+  try {
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    //Return an error if no user with the given id exists in the db
+    if (!user) {
+      return res.status(400).send("No such user exists");
+    }
+    console.log(user)
+    let userData = req.body;
+    let deletee = userData.isDeleted
+    console.log(userData)
+    let DeleteUser = await userModel.findOneAndUpdate(userId, { $set: userData }, { new: true });
+    res.status(201).send({ status: DeleteUser, data: userData });
   }
- console.log(user)
-  let userData = req.body;
-  let deletee = userData.isDeleted
-  console.log(userData)
-  let DeleteUser = await userModel.findOneAndUpdate(userId,{ $set:userData} ,{new:true});
-  res.send({ status: DeleteUser, data: userData });
-  
+  catch (errer) {
+    res.status(500).send({ mag: errer.message })
+  }
+
 };
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
-module.exports.DeletUser=DeletUser
+module.exports.DeletUser = DeletUser
 
 /*
 Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
